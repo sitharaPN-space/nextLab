45da5ae0e47b67b55b21ca0e65a66931ac46dbd0
Sithara <sitharapitiyage@gmail.com>
2024-04-02 14:29:10 +0530

    intial code setup

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const page = (props: Props) => {
+  return <p>Customers Page</p>;
+};
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+type Props = {};
+
+const page = (props: Props) => {
+  return <p>Invoices Page</p>;
+};
+
+export default page;
