77a4e1fa3a1afbe4674a54fe21b49d7002d70c00
Sithara <sitharapitiyage@gmail.com>
2024-04-02 14:39:21 +0530

    fixed image errors

--- app/page.tsx
+++ app/page.tsx
@@ -43,6 +43,7 @@ export default function Page() {
             width={560}
             height={620}
             className="block md:hidden"
+            alt="hero mobile image"
           />
         </div>
       </div>
